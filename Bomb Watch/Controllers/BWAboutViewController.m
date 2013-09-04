f3c1fd1854c0aec15a8c9d898f317577fd3b8a61
Paul Friedman <paulrfri@gmail.com>
2013-09-04 09:14:57 -0400

    Tweetbot/Twitteriffic URL schemes on about page

--- Bomb Watch/Controllers/BWAboutViewController.m
+++ Bomb Watch/Controllers/BWAboutViewController.m
@@ -15,6 +15,7 @@
 #define kMailCell       1
 
 //static NSURL *mailURL = [NSURL URLWithString:@"https://twitter.com/bombwatch"];
+static NSString *twitterName = @"bombwatch";
 
 @implementation BWAboutViewController
 
@@ -25,9 +26,20 @@
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     if (indexPath.section == kTwitterSection && indexPath.row == kTwitterCell) {
-        // TODO: tweetbot > twitterapp > twitterweb
-        NSURL *twitterURL = [NSURL URLWithString:@"https://twitter.com/bombwatch"];
-        [[UIApplication sharedApplication] openURL:twitterURL];
+
+        NSURL *tweetbotURL = [NSURL URLWithString:[NSString stringWithFormat:@"tweetbot://%@/timeline", twitterName]];
+        NSURL *twitterifficURL = [NSURL URLWithString:[NSString stringWithFormat:@"twitteriffic://account/%@/tweets", twitterName]];
+        NSURL *twitterAppURL = [NSURL URLWithString:[NSString stringWithFormat:@"twitter:@%@", twitterName]];
+        NSURL *twitterURL = [NSURL URLWithString:[NSString stringWithFormat:@"https://twitter.com/%@", twitterName]];
+        if ([[UIApplication sharedApplication] canOpenURL:tweetbotURL]) {
+            [[UIApplication sharedApplication] openURL:tweetbotURL];
+        } else if ([[UIApplication sharedApplication] canOpenURL:twitterifficURL]) {
+            [[UIApplication sharedApplication] openURL:twitterifficURL];
+        } else if ([[UIApplication sharedApplication] canOpenURL:twitterAppURL]) {
+            [[UIApplication sharedApplication] openURL:twitterAppURL];
+        } else
+            [[UIApplication sharedApplication] openURL:twitterURL];
+
     } else if (indexPath.section == kMailSection && indexPath.row == kMailCell) {
         NSURL *mailURL = [NSURL URLWithString:@"mailto:cosmonautics@laika.io"];
         [[UIApplication sharedApplication] openURL:mailURL];
