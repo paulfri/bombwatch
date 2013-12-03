d8aa88faa3e197db4a1d8f3dcfaa8e1331c1c000
Paul Friedman <paulrfri@gmail.com>
2013-12-02 21:50:10 -0500

    Add downloads view controller

---
+++ Bomb Watch/View Controllers/List/BWDownloadsViewController.h
@@ -0,0 +1,16 @@
+//
+//  BWDownloadsViewController.h
+//  Bomb Watch
+//
+//  Created by Paul Friedman on 12/2/13.
+//  Copyright (c) 2013 Laika Cosmonautics. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PDGesturedTableView.h"
+
+@interface BWDownloadsViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet PDGesturedTableView *tableView;
+
+@end
---
+++ Bomb Watch/View Controllers/List/BWDownloadsViewController.m
@@ -0,0 +1,19 @@
+//
+//  BWDownloadsViewController.m
+//  Bomb Watch
+//
+//  Created by Paul Friedman on 12/2/13.
+//  Copyright (c) 2013 Laika Cosmonautics. All rights reserved.
+//
+
+#import "BWDownloadsViewController.h"
+
+@implementation BWDownloadsViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.tableView.tableFooterView = [[UIView alloc] init];
+}
+
+@end
