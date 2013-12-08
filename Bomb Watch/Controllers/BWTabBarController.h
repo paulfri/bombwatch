dbee7bc6498f72b2dbbd2adf5a497602f5171e92
Paul Friedman <paulrfri@gmail.com>
2013-12-07 19:14:10 -0500

    Fix rotation

---
+++ Bomb Watch/Controllers/BWTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  BWTabBarController.h
+//  Bomb Watch
+//
+//  Created by Paul Friedman on 12/7/13.
+//  Copyright (c) 2013 Laika Cosmonautics. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BWTabBarController : UITabBarController
+
+@end
---
+++ Bomb Watch/Controllers/BWTabBarController.m
@@ -0,0 +1,18 @@
+//
+//  BWTabBarController.m
+//  Bomb Watch
+//
+//  Created by Paul Friedman on 12/7/13.
+//  Copyright (c) 2013 Laika Cosmonautics. All rights reserved.
+//
+
+#import "BWTabBarController.h"
+
+@implementation BWTabBarController
+
+- (BOOL)shouldAutorotate
+{
+    return NO;
+}
+
+@end
