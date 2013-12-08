cebd8ad4fc96cf71696bfb33d5ed57a1e8e6ea57
Paul Friedman <paulrfri@gmail.com>
2013-12-07 22:43:25 -0500

    add a bunch of watch progress/complete stuff to settings

--- Bomb Watch/Model/BWSettings.m
+++ Bomb Watch/Model/BWSettings.m
@@ -12,11 +12,15 @@ NSString *const kBWSettingsKeyDefaultQuality = @"kBWSettingsKeyDefaultQuality";
 NSString *const kBWSettingsKeyAPIKey = @"kBWSettingsKeyAPIKey";
 NSString *const kBWSettingsKeyLockRotation = @"kBWSettingsKeyLockRotation";
 NSString *const kBWSettingsKeyiCloudSync = @"kBWSettingsKeyiCloudSync";
+NSString *const kBWSettingsKeyWatchComplete = @"kBWSettingsKeyWatchComplete";
+NSString *const kBWSettingsKeyWatchProgress = @"kBWSettingsKeyWatchProgress";
 
 #define kBWDefaultSettingDefaultQuality  @2 // BWVideoQualityHigh
 NSString *const kBWDefaultSettingAPIKey  = @"e5ab8850b03bcec7ce6590ca705c9a26395dddf1";
 #define kBWDefaultSettingLockRotation    @YES
 #define kBWDefaultSettingsiCloudSync     @NO
+#define kBWDefaultSettingsWatchComplete  @[]
+#define kBWDefaultSettingsWatchProgress  @{}
 
 @implementation BWSettings
 
@@ -26,8 +30,8 @@ NSString *const kBWDefaultSettingAPIKey  = @"e5ab8850b03bcec7ce6590ca705c9a26395
                                   kBWSettingsKeyDefaultQuality : kBWDefaultSettingDefaultQuality,
                                     kBWSettingsKeyLockRotation : kBWDefaultSettingLockRotation,
                                       kBWSettingsKeyiCloudSync : kBWDefaultSettingsiCloudSync,
-                                          @"videosWatched": @[],
-                                          @"videoProgress": @{}};
+                                   kBWSettingsKeyWatchComplete : kBWDefaultSettingsWatchComplete,
+                                   kBWSettingsKeyWatchProgress : kBWDefaultSettingsWatchProgress};
 
     [[NSUserDefaults standardUserDefaults] registerDefaults:defaultPreferences];
 }
@@ -51,6 +55,20 @@ NSString *const kBWDefaultSettingAPIKey  = @"e5ab8850b03bcec7ce6590ca705c9a26395
     return [[NSUserDefaults standardUserDefaults] stringForKey:kBWSettingsKeyAPIKey];
 }
 
++ (BOOL)watchedVideo:(BWVideo *)video
+{
+    return [[[NSUserDefaults standardUserDefaults] arrayForKey:kBWSettingsKeyWatchComplete] containsObject:[NSNumber numberWithInt:video.videoID]];
+}
+
++ (NSTimeInterval)progressForVideo:(BWVideo *)video
+{
+    if (![[[[NSUserDefaults standardUserDefaults] dictionaryForKey:kBWSettingsKeyWatchProgress] allKeys] containsObject:[NSNumber numberWithInt:video.videoID]]) {
+        return 0.0f;
+    }
+
+    return [[[NSUserDefaults standardUserDefaults] dictionaryForKey:kBWSettingsKeyWatchProgress][[NSNumber numberWithInt:video.videoID]] doubleValue];
+}
+
 #pragma mark - setters
 
 + (void)setDefaultQuality:(BWVideoQuality)quality
@@ -68,7 +86,45 @@ NSString *const kBWDefaultSettingAPIKey  = @"e5ab8850b03bcec7ce6590ca705c9a26395
     [[NSUserDefaults standardUserDefaults] setObject:key forKey:kBWSettingsKeyAPIKey];
 }
 
-#pragma mark - misc
++ (void)addWatchedVideo:(BWVideo *)video
+{
+    NSArray *watched = [[NSUserDefaults standardUserDefaults] arrayForKey:kBWSettingsKeyWatchComplete];
+    if (![watched containsObject:video]) {
+        [[NSUserDefaults standardUserDefaults] setObject:[watched arrayByAddingObject:video] forKey:kBWSettingsKeyWatchComplete];
+    }
+}
+
++ (void)removeWatchedVideo:(BWVideo *)video
+{
+    NSMutableArray *watched = [[[NSUserDefaults standardUserDefaults] arrayForKey:kBWSettingsKeyWatchComplete] mutableCopy];
+    if ([watched containsObject:video]) {
+        [watched removeObject:video];
+    }
+
+    [[NSUserDefaults standardUserDefaults] setObject:watched forKey:kBWSettingsKeyWatchComplete];
+}
+
++ (void)removeWatchedProgressForVideo:(BWVideo *)video
+{
+    NSMutableDictionary *dict = [[[NSUserDefaults standardUserDefaults] dictionaryForKey:kBWSettingsKeyWatchProgress] mutableCopy];
+    NSNumber *key = [NSNumber numberWithInteger:video.videoID];
+    if ([[dict allKeys] containsObject:key]) {
+        [dict removeObjectForKey:key];
+    }
+
+    [[NSUserDefaults standardUserDefaults] setObject:dict forKey:kBWSettingsKeyWatchProgress];
+}
+
++ (void)setWatchedProgress:(NSTimeInterval)progress forVideo:(BWVideo *)video
+{
+    NSMutableDictionary *dict = [[[NSUserDefaults standardUserDefaults] dictionaryForKey:kBWSettingsKeyWatchProgress] mutableCopy];
+    NSNumber *key = [NSNumber numberWithInteger:video.videoID];
+    dict[key] = [NSNumber numberWithDouble:progress];
+
+    [[NSUserDefaults standardUserDefaults] setObject:dict forKey:kBWSettingsKeyWatchProgress];
+}
+
+#pragma mark - Account linkage
 
 + (BOOL)accountIsLinked
 {
